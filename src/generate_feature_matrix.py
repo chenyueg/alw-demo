908449a1dad874e865f2e049fd660389df51e7c3
Eleanor Chen <chenyueg@gmail.com>
2021-03-21 21:48:51 -0400

    finished the read-in functions

---
+++ src/generate_feature_matrix.py
@@ -0,0 +1,39 @@
+import string
+
+# File paths
+tweetsFile = "../raw_data/retrieved_tweets_trial.txt"
+wordsFile = "../features/abusive_words.txt"
+
+def process_tweets(tweets):
+
+    cleanTweetsWithLabels = []
+
+    for each in tweets:
+        tweet = each.strip("\n").split(" ")
+        number = tweet.pop(0)
+        tweetID = tweet.pop(0)
+        label = tweet.pop()
+        tweet = [word.strip(string.punctuation).lower() for word in tweet]
+        # print([number, tweetID, tweet, label])
+        cleanTweetsWithLabels.append([number, tweetID, tweet, label])
+
+    return cleanTweetsWithLabels
+
+def process_words(words):
+
+    wordList = []
+
+    for word in words:
+        wordList.append(word.strip("\n").lower())
+
+    return wordList
+
+with open(tweetsFile, encoding = "utf-8") as tweetsHandle:
+    tweets = tweetsHandle.readlines()
+with open(wordsFile, encoding = "utf-8") as wordsHandle:
+    words = wordsHandle.readlines()
+
+cleanTweetsWithLabels = process_tweets(tweets)
+# print(cleanTweetsWithLabels)
+wordList = process_words(words)
+# print(wordList)
