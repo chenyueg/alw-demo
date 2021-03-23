9bd2e3b29e91cd37402d93e6a28ecc02cb19b816
Yue Chen <yc59@indiana.edu>
2021-03-23 19:34:18 -0400

    finished the write-out functions

--- src/generate_feature_matrix.py
+++ src/generate_feature_matrix.py
@@ -1,11 +1,17 @@
 import string
 
 # File paths
-tweetsFile = "../raw_data/retrieved_tweets_trial.txt"
-wordsFile = "../features/abusive_words.txt"
+tweetsFile = "../raw_data/retrieved_tweets.txt"
+wordsFile = "../raw_data/abusive_words.txt"
+timblTrainFile = "../features/timbl_feature_matrix_train"
+timblTestFile = "../features/timbl_feature_matrix_test"
 
 def process_tweets(tweets):
 
+    '''
+    Return a list of pre-processed tweets with the format being [number, tweetID, tweet, label]
+    '''
+    
     cleanTweetsWithLabels = []
 
     for each in tweets:
@@ -38,7 +44,12 @@ def generate_matrix(tweets, words):
         for i in range(wordsLentgh):
             # tweet[2] is the sub-list that contains the actual tweet
             featureVector[i] = tweet[2].count(words[i])
+            # tweet[3] is the label
+        featureVector.append(tweet[3])
+        # print(tweet[3])
         featureMatrix.append(featureVector)
+
+    # print(featureMatrix)
 
     return featureMatrix
 
@@ -54,3 +65,23 @@ wordList = process_words(words)
 featureMatrix = generate_matrix(cleanTweetsWithLabels, wordList)
 # print(len(featureMatrix), len(featureMatrix[0]))
 # print(featureMatrix)
+
+timblTrainHandle = open(timblTrainFile, "w+", encoding = "utf-8")
+timblTestHandle = open(timblTestFile, "w+", encoding = "utf-8")
+
+count = 1
+
+for eachTweet in featureMatrix:
+
+    # Change 9 to 5 to create a 20% test set
+    if count % 9:
+        timblTrainHandle.write(",".join([str(value) for value in eachTweet]))
+        timblTrainHandle.write("\n")
+    else:
+        timblTestHandle.write(",".join([str(value) for value in eachTweet]))
+        timblTestHandle.write("\n")
+
+    count += 1
+
+timblTrainHandle.close()
+timblTestHandle.close()
